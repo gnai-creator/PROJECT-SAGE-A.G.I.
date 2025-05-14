135bab795470364a98ef56366195defa5adf016d
Felipe Maya Muniz <67922105+gnai-creator@users.noreply.github.com>
2025-05-14 20:27:47 -0300

    Create sage14_AGI.py

---
+++ sage14_AGI.py
@@ -0,0 +1,92 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Dense, LayerNormalization, MultiHeadAttention, GRUCell, Conv2D, Flatten, Input, Softmax
+
+class ValueSystem(tf.keras.layers.Layer):
+    def __init__(self, dim):
+        super().__init__()
+        self.value_vector = tf.Variable(tf.random.normal([1, dim]), trainable=False)
+        self.internal_ethics = Dense(dim, activation='tanh')
+        self.alignment_gate = Dense(1, activation='sigmoid')
+        self.sensitivity = tf.Variable(tf.ones([1, dim]), trainable=True)
+
+    def call(self, x):
+        projection = self.internal_ethics(x)
+        gate = self.alignment_gate(x)
+        updated_value = 0.9 * self.value_vector + 0.1 * projection
+        self.value_vector.assign(updated_value)
+        ethical_aligned = x * (1 - gate) + self.value_vector * gate
+        pain_signal = self.sensitivity * tf.square(x - self.value_vector)
+        return ethical_aligned, gate, pain_signal
+
+class EthicalConflict(tf.keras.layers.Layer):
+    def __init__(self):
+        super().__init__()
+        self.accumulated_pain = tf.Variable(0.0, trainable=False)
+
+    def call(self, action, value, output):
+        conflict = tf.abs(action - value)
+        score = tf.reduce_mean(conflict)
+        self.accumulated_pain.assign_add(score)
+        return score + self.accumulated_pain * 0.01
+
+class ReflectiveMoralAgent(tf.keras.layers.Layer):
+    def __init__(self, dim):
+        super().__init__()
+        self.cell = GRUCell(dim)
+        self.state = tf.Variable(tf.zeros([1, dim]), trainable=False)
+        self.reflect = Dense(dim, activation='relu')
+
+    def call(self, x):
+        x = self.reflect(tf.reduce_mean(x, axis=1))
+        out, state = self.cell(x, [self.state])
+        self.state.assign(state[0])
+        return out
+
+class ARCMetaHypothesis(tf.keras.layers.Layer):
+    def __init__(self, dim):
+        super().__init__()
+        self.hypotheses = [Dense(dim) for _ in range(3)]
+        self.selector = Dense(1, activation='softmax')
+
+    def call(self, x):
+        candidates = [h(x) for h in self.hypotheses]
+        stacked = tf.stack(candidates, axis=1)
+        scores = self.selector(tf.reduce_mean(stacked, axis=-1))
+        return tf.reduce_sum(stacked * tf.expand_dims(scores, -1), axis=1)
+
+class VisualPatternAdapter(tf.keras.layers.Layer):
+    def __init__(self, hidden_dim):
+        super().__init__()
+        self.conv = Conv2D(hidden_dim, (3, 3), activation='relu', padding='same')
+        self.flatten = Flatten()
+        self.adapter = Dense(hidden_dim, activation='relu')
+
+    def call(self, img):
+        feat = self.conv(img)
+        flat = self.flatten(feat)
+        return self.adapter(flat)
+
+class Sage14AGI(tf.keras.Model):
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        super().__init__()
+        self.encoder = Dense(hidden_dim, activation='relu')
+        self.attn = MultiHeadAttention(num_heads=8, key_dim=8)
+        self.norm = LayerNormalization()
+        self.agent = ReflectiveMoralAgent(hidden_dim)
+        self.value_system = ValueSystem(hidden_dim)
+        self.ethical_conflict = EthicalConflict()
+        self.hypothesis = ARCMetaHypothesis(hidden_dim)
+        self.decoder = Dense(output_dim)
+
+    def call(self, x):
+        tf.debugging.assert_rank(x, 2)
+        x = self.encoder(x)
+        x = tf.expand_dims(x, 1)
+        x = self.attn(x, x, x)
+        x = self.norm(x)
+        agent_out = self.agent(x)
+        aligned, gate, pain_signal = self.value_system(agent_out)
+        hypothesis = self.hypothesis(agent_out)
+        conflict_score = self.ethical_conflict(agent_out, self.value_system.value_vector, hypothesis)
+        output = self.decoder(aligned)
+        return output, conflict_score, gate, self.value_system.value_vector, pain_signal
